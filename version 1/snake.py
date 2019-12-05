7fc844eb35c489afe3a558071f49932a4229ff7e
maharaj4.0 <mkumar2@cs.iitr.ac.in>
2019-12-05 17:54:39 +0530

    snake is ready to rrock now food is to be created

--- version 1/snake.py
+++ version 1/snake.py
@@ -19,6 +19,10 @@ top_left_y = s_height - play_height # top left y of play area
 
  ## GLOBAL SCORE VARS
 score = [0,0,0]
+
+# snake details
+snake = [(top_left_x + play_width/2 , top_left_y+ play_height/2)]
+direction = 1
  
 #colors of food that will be generated 
 food_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
@@ -84,23 +88,103 @@ def draw_window(surface):
     #         pygame.draw.rect(surface, grid[i][j], (top_left_x + j* block_size, top_left_y + i * block_size, block_size, block_size), 0)
  
     # draw mesh 
-    # IF YOU WANT TO DISABLE THE MESH JUST COMMENT THE LINE OF CODE BELOW
-    draw_mesh(surface, 30, 30)
+    # IF YOU WANT TO DISABLE / DRAW THE MESH JUST COMMENT / UNCOMMENT THE LINE OF CODE BELOW
+    # draw_mesh(surface, 30, 30)
     #border
     pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, play_width, play_height), 5)
     # pygame.display.update()
  
+
+
+def is_next_move_valid(x , y):
+    global snake
+
+    if len(snake)>0:
+        for index in range(len(snake)-1):
+            item = snake[index]
+            if x == item[0] and y == item[1]:
+                return False
+
+    if ( x >= top_left_x+play_width + 3* block_size )   or (x <= top_left_x + 1 * block_size   ) :
+        return False
+    if (y >= top_left_y+play_height  - 2* block_size) or (y <= top_left_y - 4 *block_size) :
+        return False
+
+    return True              
+
+
+
+def make_snake_move(surface):
+    global snake
+    global direction
+    
+    # draw_text_middle('make snake move', 50, (255,255,255), win)
+    # pygame.display.update()
+    # pygame.time.delay(2000)
+    # draw_text_middle('make snake move' ,50, (0,0,0), win)
+
+    pygame.display.update()
+    #remove last snake block
+    item = snake[-1]
+    x = item[0]
+    y = item[1]
+    #enter next snake's loaction
+    try :
+        item = snake[0]
+        x_new = item[0]
+        y_new = item[1]
+    except :
+        x_new = x
+        y_new = y
+    if direction == 0: # move right
+        y_new += block_size
+    elif direction == 1: # move up
+        x_new -= block_size   
+    elif direction == 2 : #move l
+        y_new -= block_size
+    elif direction == 3 :
+        x_new += block_size
+    else :# some weired thing happend and we need to check
+        x_new+=1
+        draw_text_atXY('WARNING', 30, (255, 255, 255), surface ,play_width + 70 ,play_height/2 +70, False)
+        draw_text_atXY('direction is '+str(direction), 10, (255, 255, 255), surface ,play_width + 70,play_height/2 +70, False)
+    
+    
+    if is_next_move_valid(x_new , y_new) :
+
+
+        # draw_text_middle(''+str(x_new) + " " + str(y_new), 50, (255,255,255), win)
+        # pygame.display.update()
+        # pygame.time.delay(2000)
+        # draw_text_middle(''+str(x_new) + " " + str(y_new), 50, (0,0,0), win)
+
+        # add new point to the snake
+        snake.insert(0,(x_new , y_new))
+        #remove last element in snake
+        snake.pop()
+        # draw the new snake block on the screen
+        pygame.draw.rect(surface, snake_color, ( y_new, x_new, block_size, block_size), 0)     
+
+        # delete the last snake block 
+        pygame.draw.rect(surface, (0,0,0), ( y,  x, block_size, block_size), 0)
+        
+        return True
+
+    return False        
+
+
+
+
  
 def main():
-    global grid
-    global score
     
+    # global score
+     # snake this is list which contains the coordinates where snake currently is 
+    global snake
     # if direction is 0 (right) , 1(up) , 2(left ) or 3 (down)
-    direction = 1
+    global direction 
 
-    # snake this is list which contains the coordinates where snake currently is 
-    snake = [(top_left_x + play_width/2 , top_left_y+ play_height/2)]
-    
+   
 
     # to run the game  
     run = True
@@ -110,24 +194,30 @@ def main():
 
     # time to make snake move
     move_time = 0
-    
+
     # snakes's speed 
     move_speed = 0.27
 
-    while run:
+    draw_window(win)
+    # pygame.draw.rect(win, snake_color, ( top_left_x + 10, top_left_y +10* block_size, block_size, block_size), 0)
+    valid = make_snake_move(win)
+    # draw_text_middle("valid is " +str(valid), 50, (255,255,255), win)
+    # pygame.display.update()
+    # pygame.time.delay(2000)
+    # draw_text_middle("valid is " +str(valid), 50, (0,0,0), win)
+
+    while run and valid:
         
  
         move_time += clock.get_rawtime()
         clock.tick()
  
-        # PIECE moveING CODE
+        # SNAKE MOVING CODE
         if move_time/1000 >= move_speed:
             move_time = 0
-            current_piece.y += 1
-            if not (valid_space(current_piece, grid)) and current_piece.y > 0:
-                current_piece.y -= 1
-                change_piece = True
- 
+            valid = make_snake_move(win)
+            
+    
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -136,66 +226,40 @@ def main():
  
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    current_piece.x -= 1
-                    if not valid_space(current_piece, grid):
-                        current_piece.x += 1
- 
+                    direction = 2 # 2 is left
+                    
+
                 elif event.key == pygame.K_RIGHT:
-                    current_piece.x += 1
-                    if not valid_space(current_piece, grid):
-                        current_piece.x -= 1
+                    direction = 0 # 0 is right
+                    
+
                 elif event.key == pygame.K_UP:
-                    # rotate shape
-                    current_piece.rotation = current_piece.rotation + 1 % len(current_piece.shape)
-                    if not valid_space(current_piece, grid):
-                        current_piece.rotation = current_piece.rotation - 1 % len(current_piece.shape)
- 
+                    direction = 1 # 1 is up
+                    
+
                 if event.key == pygame.K_DOWN:
-                    # move shape down
-                    current_piece.y += 1
-                    if not valid_space(current_piece, grid):
-                        current_piece.y -= 1
+                    direction = 3 # 3 is down
+                    
+                valid = make_snake_move(win)
+                
  
-                '''if event.key == pygame.K_SPACE:
-                   while valid_space(current_piece, grid):
-                       current_piece.y += 1
-                   current_piece.y -= 1
-                   print(convert_shape_format(current_piece))'''  # todo fix
- 
-        shape_pos = convert_shape_format(current_piece)
- 
-        # add piece to the grid for drawing
-        for i in range(len(shape_pos)):
-            x, y = shape_pos[i]
-            if y > -1:
-                grid[y][x] = current_piece.color
- 
-        # IF PIECE HIT GROUND
-        if change_piece:
-            for pos in shape_pos:
-                p = (pos[0], pos[1])
-                snake[p] = current_piece.color
-            current_piece = next_piece
-            next_piece = get_shape()
-            change_piece = False
- 
-            # call four times to check for multiple clear rows
-            score[0] +=  clear_rows(grid, snake)*10
         
-        draw_window(win)
-        draw_next_shape(next_piece, win)
-        draw_text_atXY('Current Score : '+str(score[0]), 35, (255, 255, 255), win ,play_width + 120,play_height/2 +70, False)
-        draw_text_atXY('Best Score : '+str(score[2]), 25, (255, 255, 255), win ,play_width + 120,play_height/2 +90, False)
-        draw_text_atXY('2nd Best Score : '+str(score[1]), 20, (255, 255, 255), win ,play_width + 118,play_height/2 +110, False)
+        
+        
+        
+        # draw_text_atXY('Current Score : '+str(score[0]), 35, (255, 255, 255), win ,play_width + 120,play_height/2 +70, False)
+        # draw_text_atXY('Best Score : '+str(score[2]), 25, (255, 255, 255), win ,play_width + 120,play_height/2 +90, False)
+        # draw_text_atXY('2nd Best Score : '+str(score[1]), 20, (255, 255, 255), win ,play_width + 118,play_height/2 +110, False)
         pygame.display.update()
  
-        # Check if user lost
-        if check_lost(snake):
-            run = False
+        
  
+    
+    # score.sort()
+    # score[0]=0
     draw_text_middle("You Lost !!", 50, (255,255,255), win)
-    score.sort()
-    score[0]=0
+    snake = [(top_left_x + play_width/2 , top_left_y+ play_height/2)]
+    direction = 1
     pygame.display.update()
     pygame.time.delay(2000)
  
@@ -224,6 +288,6 @@ def main_menu():
  
  
 win = pygame.display.set_mode((s_width, s_height))
-pygame.display.set_caption('Tetris')
+pygame.display.set_caption('Snake')
  
 main_menu()  
